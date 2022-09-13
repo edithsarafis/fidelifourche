6f2888b73385f0c38b7a1f99a9e13cd1c25be1f3
Edith Sarafis <edith.sarafis@zelros.com>
2022-09-13 20:56:40 +0200

    added more preprocessing steps in clean_data

--- fidelifourche/data.py
+++ fidelifourche/data.py
@@ -16,12 +16,39 @@ def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     """
 
     print(f"\n Shape before cleaning : {df.shape}")
-    #drop duplicates
+
+    ## Filter data
+    #Filter created_at : starting from 01/01/2020
+
+    df=df.loc[df['created_at']>='2020',:]
+
+    ## Clean data
+    ### Drop duplicates
+
     df.drop_duplicates(inplace=True)
 
-    #drop irrelevant values
+    ### Drop missing values for ZIP code
 
-    #replace missing values
+    df.dropna(axis=0, subset='zip', inplace=True)
+
+    ### Replace outliers for DELAY
+    # Edith -> peut etre plutot -1 pour qu'on différencie avec des vraies delay = 0 ?
+
+    df['delay'].replace([-3367,-3366,-3247,-3244,-709,-703],0,inplace=True)
+
+    ## Feature Engineering
+    ### Convert to datetime
+
+    df.loc[:,'created_at']= pd.to_datetime(df['created_at'])
+    df.loc[:,'ticket_at']= pd.to_datetime(df['ticket_at'])
+
+    ### Transform weight (from g en Kg)
+
+    df.loc[:,'weight']=df['weight']/1000
+
+    ### Create a new column : department
+
+    df.loc[:,'department'] = df['zip'].str[:2]
 
     print(f"\n Shape after cleaning : {df.shape}")
     print("\n✅ data cleaned")
