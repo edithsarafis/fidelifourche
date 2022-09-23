45d33388c25c74484853e28c90ad39388323334a
Edith Sarafis <edith.sarafis@zelros.com>
2022-09-23 16:46:26 +0200

    change of functions name in main and added preprocess pipeline

--- fidelifourche/main.py
+++ fidelifourche/main.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 from fidelifourche.preproc import preprocess_features
 
-def preprocess():
+def clean_merge():
 
     # Load data
     orders,details,sav,nb_epicerie_bio,zip_invalid = load_data()
@@ -20,6 +20,12 @@ def preprocess():
     # Merge zip data
     df = merge_zip(df_clean,nb_epicerie_bio,zip_invalid)
 
+    print("✅ data cleaned and merged")
+
+    return df
+
+def preprocess(df:pd.DataFrame):
+
     # Create X, y
     X = df.drop("bool_churn", axis=1)
     y = df[["bool_churn"]]
@@ -29,12 +35,13 @@ def preprocess():
 
     print("✅ data preprocessed")
 
-    return df,X_preproc,y
+    return X_preproc,y
 
 
 if __name__ == '__main__':
     try:
-        preprocess()
+        df = clean_merge()
+        X_preproc,y = preprocess(df)
     except:
         import ipdb, traceback, sys
         extype, value, tb = sys.exc_info()
